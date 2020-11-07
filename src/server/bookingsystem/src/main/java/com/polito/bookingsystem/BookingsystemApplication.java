f9d8ff5f9082e101ccc88920f283f7ef2c8c7ae9
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-11-07 17:13:39 +0200

    feat: setup backend project skeleton

---
+++ src/server/bookingsystem/src/main/java/com/polito/bookingsystem/BookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.polito.bookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingsystemApplication.class, args);
+	}
+
+}
---
+++ src/server/bookingsystem/src/test/java/com/polito/bookingsystem/BookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polito.bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
