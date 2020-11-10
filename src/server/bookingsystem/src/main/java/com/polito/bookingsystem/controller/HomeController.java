9014275cf1cd5b4624a056903b300950db294ec5
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-11-10 16:56:26 +0200

    feat: remove security

--- src/server/bookingsystem/src/main/java/com/polito/bookingsystem/controller/HomeController.java
+++ src/server/bookingsystem/src/main/java/com/polito/bookingsystem/controller/HomeController.java
@@ -1,9 +1,17 @@
 package com.polito.bookingsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String test() {
+		return "test";
+	}
 
 }
