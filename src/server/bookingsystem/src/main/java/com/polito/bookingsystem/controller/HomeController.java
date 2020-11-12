27e343a0d1755ad940686bdf6aec6d6d4650b299
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-11-12 15:46:22 +0200

    Fix: fix whitelabel error

--- src/server/bookingsystem/src/main/java/com/polito/bookingsystem/controller/HomeController.java
+++ src/server/bookingsystem/src/main/java/com/polito/bookingsystem/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.polito.bookingsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String test() {
+		return "test";
+	}
 }
