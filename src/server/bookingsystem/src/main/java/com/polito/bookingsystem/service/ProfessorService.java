56392c5390241bcd97ef3b50a2a615229f1e1950
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-11-16 02:19:00 +0200

    feat: Design email notification system

--- src/server/bookingsystem/src/main/java/com/polito/bookingsystem/service/ProfessorService.java
+++ src/server/bookingsystem/src/main/java/com/polito/bookingsystem/service/ProfessorService.java
@@ -1,4 +1,11 @@
 package com.polito.bookingsystem.service;
 
+import org.springframework.stereotype.Service;
+
+import com.polito.bookingsystem.dto.ProfessorDto;
+
+@Service
 public interface ProfessorService {
+	
+	public void sendEmail(ProfessorDto professorDto, String subject, String text);
 }
--- src/server/bookingsystem/src/main/java/com/polito/bookingsystem/service/impl/ProfessorServiceImpl.java
+++ src/server/bookingsystem/src/main/java/com/polito/bookingsystem/service/impl/ProfessorServiceImpl.java
@@ -1,9 +1,28 @@
 package com.polito.bookingsystem.service.impl;
 
 
+import com.polito.bookingsystem.dto.ProfessorDto;
 import com.polito.bookingsystem.service.ProfessorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfessorServiceImpl implements ProfessorService {
+	
+	@Autowired
+    private JavaMailSender javaMailSender;
+	
+	@Override
+	public void sendEmail(ProfessorDto professorDto, String subject, String text) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(professorDto.getEmail());
+
+        msg.setSubject(subject);
+        msg.setText(text);
+
+        javaMailSender.send(msg);
+	}
 }
