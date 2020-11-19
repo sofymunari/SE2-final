61df0f2fcefc9cf71c5dbce15a892cb302f4209c
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-11-19 13:18:21 +0200

    feat: Add send notification to professor to inform about the number of bookings for a lecture

--- src/server/bookingsystem/src/main/java/com/polito/bookingsystem/repository/BookingRepository.java
+++ src/server/bookingsystem/src/main/java/com/polito/bookingsystem/repository/BookingRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import com.polito.bookingsystem.entity.Booking;
+import com.polito.bookingsystem.entity.Lecture;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Integer>{
 
 	Booking findByBookingId(Integer bookingId);
+	List<Booking> findByLecture(Lecture lecture);
+	
 	
 	@Query("SELECT B.bookingId as bookingId, B.bookingInfo as bookingInfo, S.name as studentName, S.surname as studentSurname, S.email as studentEmail, L.lectureId as lectureId, L.date as lectureDate, L.numberOfLesson as lectureNumber, L.course as course " +
 			"FROM Booking B JOIN B.lecture L JOIN L.professor P JOIN B.student S WHERE P.email = ?1")
--- src/server/bookingsystem/src/main/java/com/polito/bookingsystem/service/impl/BookingServiceImpl.java
+++ src/server/bookingsystem/src/main/java/com/polito/bookingsystem/service/impl/BookingServiceImpl.java
@@ -7,16 +7,19 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.polito.bookingsystem.converter.BookingConverter;
+import com.polito.bookingsystem.converter.ProfessorConverter;
 import com.polito.bookingsystem.converter.StudentConverter;
 import com.polito.bookingsystem.dto.BookingDto;
 import com.polito.bookingsystem.entity.Booking;
 import com.polito.bookingsystem.entity.Lecture;
+import com.polito.bookingsystem.entity.Professor;
 import com.polito.bookingsystem.entity.Student;
 import com.polito.bookingsystem.repository.BookingEntry;
 import com.polito.bookingsystem.repository.BookingRepository;
 import com.polito.bookingsystem.repository.LectureRepository;
 import com.polito.bookingsystem.repository.StudentRepository;
 import com.polito.bookingsystem.service.BookingService;
+import com.polito.bookingsystem.service.NotificationProfessorService;
 import com.polito.bookingsystem.service.StudentService;
 import com.polito.bookingsystem.utils.BookingInfo;
 
@@ -35,6 +38,9 @@ public class BookingServiceImpl implements BookingService{
 	
 	@Autowired
 	private StudentService studentService;
+	
+	@Autowired
+	private NotificationProfessorService notificationProfessorService;
 	
 	
 	@Autowired
@@ -98,6 +104,10 @@ public class BookingServiceImpl implements BookingService{
 		bookingRepository.save(booking);
 		String text = "Dear "+student.getName()+" your booking for lecture "+lecture.getCourse().getName()+" has been confirmed";
 		studentService.sendEmail(StudentConverter.toDto(student), "booking confirmation", text);
+		
+		Professor professor = lecture.getProfessor();
+		String description = "You now have "+ (bookingRepository.findByLecture(lecture)).size() + "for lecture "+lecture.getCourse().getName();
+		notificationProfessorService.sendProfessorNotification(ProfessorConverter.toDto(professor), description, "");
 		
 		return BookingConverter.toDto(booking);		             
 	}
