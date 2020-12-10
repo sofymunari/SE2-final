055c1be34e5a75bcefed80337b6c0526408cafe2
s277922 <fiorentino.cairone@gmail.com>
2020-12-10 09:36:32 +0100

    merge from main

--- src/server/bookingsystem/src/main/resources/data.sql
+++ src/server/bookingsystem/src/main/resources/data.sql
@@ -76,7 +76,5 @@ INSERT INTO NOTIFICATION_PROFESSOR(NOTIFICATION_ID,DATE,DESCRIPTION,LINK,STATUS,
 
 
 
-INSERT INTO MANAGER(USER_ID,NAME,SURNAME,ADDRESS,EMAIL,PASSWORD) VALUES (0, 'Giuseppe', 'Conte', 'Palazzo Chigi', 'manager@polito.it', 'password');
-
-
+INSERT INTO MANAGER(USER_ID,NAME,SURNAME,ADDRESS,EMAIL,PASSWORD) VALUES (0, 'Giuseppe', 'Conte', 'Palazzo Chigi', 'manager@polito.it', 'password')
 INSERT INTO OFFICER(USER_ID,NAME,SURNAME,ADDRESS,EMAIL,PASSWORD) VALUES (0, 'Mario', 'Rossi', 'Via quella 12', 'officer@polito.it', 'password');
